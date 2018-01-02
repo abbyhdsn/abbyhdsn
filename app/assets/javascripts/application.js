ece122349a09ccdcfee113da69caba6965725c80
ABBY HUDSON <ABBYHDSN@GMAIL.COM>
2018-01-02 16:01:35 -0500

    Added bootstrap, devise, set up navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+ root to: "pages#home"
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
