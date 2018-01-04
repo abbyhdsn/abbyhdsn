582763725e9c4085c87f44bc9445c48fee266b97
ABBY HUDSON <ABBYHDSN@GMAIL.COM>
2018-01-03 19:49:52 -0500

    Added timestamp to posts and fixed categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -72,6 +72,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :drafts)
+      params.require(:post).permit(:title, :body, :drafts, :category_id)
     end
 end
